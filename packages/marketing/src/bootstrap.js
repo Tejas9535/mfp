4be778b4ac0e7279a85fbd1dffeb0037d72577c1
Tejas Chokhawala <tchokhawala@ibaset.com>
2024-11-16 09:39:54 +0530

    Initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,17 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+const mount = (ele) => {    
+    ReactDOM.render(<App/>, ele);
+}
+
+if (process.env.NODE_ENV === 'development') {
+    const devRoot = document.querySelector('#_marketing-dev-root');
+    if (devRoot) {
+        mount(devRoot);
+    }
+}
+// console.log("Hello from marketing");
+
+export { mount };
