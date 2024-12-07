84c6ece3b0d1e86a2ac63fe8241f4ea8b83ad820
Tejas Chokhawala <tchokhawala@ibaset.com>
2024-12-07 11:53:04 +0530

    testing ci/cd

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 // import { mount } from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
 
-// console.log(mount);
+console.log("Hello from container");
 
 export default () => {
     return (<>
