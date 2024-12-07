0bc6fa4d2d54c8c81a2983ee79efc81d6c9e815a
Tejas Chokhawala <tchokhawala@ibaset.com>
2024-12-07 14:29:58 +0530

    fixed css pproduction issues

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,17 +1,23 @@
 import React from 'react';
-// import { mount } from 'marketing/MarketingApp';
 import MarketingApp from './components/MarketingApp';
 import { BrowserRouter } from 'react-router-dom';
 import Header from './components/Header';
+import { createGenerateClassName, StylesProvider } from '@material-ui/core/styles';
 
-// console.log("Hello from container");
+
+const generateClassName = createGenerateClassName({
+    productionPrefix: 'ca'
+})
 
 export default () => {
     return (
-        <BrowserRouter>
-        <div>
-                <Header />
-        <MarketingApp />
-            </div>
-        </BrowserRouter>)
+        <StylesProvider generateClassName={generateClassName}>
+            <BrowserRouter>
+                <div>
+                    <Header />
+                    <MarketingApp />
+                </div>
+            </BrowserRouter>
+        </StylesProvider>
+    )
 }
